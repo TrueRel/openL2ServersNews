07ff01d31b6baadcd3280e397f20948384ff4745
TrueRel <GreenGlore@i.ua>
2025-06-07 14:33:33 +0300

    first commit, add project

---
+++ src/main/java/org/example/SeleniumExample.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class SeleniumExample {
+    public static void main(String[] args) {
+        // Set path to your ChromeDriver
+        System.setProperty("webdriver.chrome.driver", "F:\\chrome-win64\\Version 137.0.7151.68\\chromedriver-win64\\chromedriver.exe");
+
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("F:\\chrome-win64\\Version 137.0.7151.68\\chrome-win64\\chrome.exe");
+        // Удалили --headless
+        options.addArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+                "(KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36");
+
+        // Настройки для Chrome
+        WebDriver driver = new ChromeDriver(options);
+        // Устанавливаем пользовательские заголовки
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        // Открываем сайт
+        System.out.println("Открываю сайт...");
+        driver.get("https://l2oops.com");
+        // Печатаем заголовок страницы
+        System.out.println("Заголовок страницы: " + driver.getTitle());
+
+
+        try {
+            //закрывать драйвер через 5 секунд
+            Thread.sleep(15000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Текущий URL: " + driver.getCurrentUrl());
+        // Закрываем драйвер
+        driver.quit();
+        System.out.println("Закрываю страницу");
+    }
+}
+
