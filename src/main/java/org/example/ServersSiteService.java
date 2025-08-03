3c051bb7fa465cc978316307024af38ba232c61f
TrueRel <GreenGlore@i.ua>
2025-08-03 19:40:05 +0300

    Добавил описание команды /help чтобы информация приходила ообратно в телеграм бот по запросу

--- src/main/java/org/example/ServersSiteService.java
+++ src/main/java/org/example/ServersSiteService.java
@@ -1,58 +1,60 @@
-package org.example;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-
-public class ServersSiteService {
-
-    private WebDriver driver;
-
-    public ServersSiteService(WebDriver driver) {
-        this.driver = driver;
-    }
-
-    public void openSite(String url) {
-        driver.get(url);
-    }
-
-
-    public List<String> getFilteredServers() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement ulElement = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("ul.flex.flex-col.gap-2"))
-        );
-
-        List<WebElement> liElements = ulElement.findElements(By.tagName("li"));
-        List<String> filtered = new ArrayList<>();
-
-        System.out.println(">>> [БС] = Бонус Старт <<<" + "\n");
-        for (WebElement li : liElements) {
-            String text = li.getText().replace("\n", " ").replace("\r", " ").trim();
-            String textLower = text.toLowerCase();
-
-            if (!text.isEmpty() &&(
-                    textLower.contains("scryde.net") ||
-                            textLower.contains("l2mad.net") ||
-                            textLower.contains("euro-pvp.net") ||
-                            textLower.contains("ketrawars.net") ||
-                            textLower.contains("e-global") ||
-                            textLower.contains("турнир") ||
-                            textLower.contains("moon-land.com") ||
-                            textLower.contains("l2etina.com") ||
-                            textLower.contains("interlude-online") ||
-                            textLower.contains("lu4")
-
-            )) {
-                filtered.add(text);
-            }
-        }
-        return filtered;
-    }
-}
+//package org.example;
+//
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.support.ui.ExpectedConditions;
+//import org.openqa.selenium.support.ui.WebDriverWait;
+//
+//import java.time.Duration;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//
+//@Deprecated
+//public class ServersSiteService {
+//
+//    private WebDriver driver;
+//
+//    public ServersSiteService(WebDriver driver) {
+//        this.driver = driver;
+//    }
+//
+//    public void openSite(String url) {
+//        driver.get(url);
+//    }
+//
+//
+//    public List<String> getFilteredServers() {
+//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//        WebElement ulElement = wait.until(
+//                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("ul.flex.flex-col.gap-2"))
+//        );
+//
+//        List<WebElement> liElements = ulElement.findElements(By.tagName("li"));
+//        List<String> filtered = new ArrayList<>();
+//
+//        System.out.println(">>> [БС] = Бонус Старт <<<" + "\n");
+//        for (WebElement li : liElements) {
+//            String text = li.getText().replace("\n", " ").replace("\r", " ").trim();
+//            String textLower = text.toLowerCase();
+//
+//            if (!text.isEmpty() &&(
+//                    textLower.contains("scryde.net") ||
+//                            textLower.contains("l2mad.net") ||
+//                            textLower.contains("euro-pvp.net") ||
+//                            textLower.contains("ketrawars.net") ||
+//                            textLower.contains("e-global") ||
+//                            textLower.contains("турнир") ||
+//                            textLower.contains("moon-land.com") ||
+//                            textLower.contains("l2etina.com") ||
+//                            textLower.contains("interlude-online") ||
+//                            textLower.contains("lu4")
+//
+//            )) {
+//                filtered.add(text);
+//            }
+//        }
+//        return filtered;
+//    }
+//}
--- src/main/java/org/example/TelegramBot/MyWebhookBot.java
+++ src/main/java/org/example/TelegramBot/MyWebhookBot.java
@@ -1,11 +1,16 @@
 package org.example.TelegramBot;
 
+import jakarta.annotation.PostConstruct;
 import org.example.ServerParserService;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.List;
 
@@ -38,12 +43,45 @@ public class MyWebhookBot extends TelegramWebhookBot {
                 SendMessage message = new SendMessage();
                 message.setChatId(chatId.toString());
                 message.setText(header + (response.length() > 4000 ? response.substring(0, 4000) : response));
-
                 return message;
+            }
+            else if (text.equals("/help")){
+                String helpInfo = "бот парсит информацию открытия серверов если пришли хоть какие то изменения на сайте" +
+
+                        "";
+                SendMessage messageHelpInfo = new SendMessage();
+                messageHelpInfo.setChatId(chatId.toString());
+                messageHelpInfo.setText(helpInfo);
+
+                return messageHelpInfo;
             }
         }
         return null;
     }
+
+    @PostConstruct
+    public void init() {
+        setBotCommands(); // установить команды при старте
+    }
+
+    public void setBotCommands() {
+        List<BotCommand> commands = List.of(
+                new BotCommand("/start", "Запустить бота"),
+                new BotCommand("/help", "Описание того что делает бот")
+        );
+
+        SetMyCommands setMyCommands = new SetMyCommands();
+        setMyCommands.setCommands(commands);
+
+        try {
+            execute(setMyCommands);
+
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
     @Override
     public String getBotPath() {
